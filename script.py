063ab5dcc5708a70e0df738895d3384119629805
Sta4ik <ctac354.354@gmail.com>
2026-05-18 22:14:38 +0300

    find fish and greenzone and simply method for fishing(need rework)

--- script.py
+++ script.py
@@ -2,6 +2,15 @@ import cv2
 import mss
 import win32gui
 import numpy as np
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+def useC(press):
+    if press:
+        keyboard.press("c")
+    else:
+        keyboard.release("c")
 
 def getWindowCoordinate(name):
     try:
@@ -17,17 +26,32 @@ def getWindowCoordinate(name):
 def getScreenGame():
     scr = mss.mss()
     template = cv2.imread("images/template.png", 0)
+    fish = cv2.imread("images/fish.png", 0)
+    greenZone = cv2.imread("images/greenzone.png", 0 )
 
     while True:
         left, top, width, height = getWindowCoordinate("Stardew Valley")
         
         screen = np.array(scr.grab({"left": left, "top": top, "width": width, "height": height}))
-        screen_bgr = cv2.cvtColor(screen, cv2.COLOR_BGRA2BGR)
-        screen_gray = cv2.cvtColor(screen_bgr, cv2.COLOR_BGR2GRAY)
+        screenGray = cv2.cvtColor(screen, cv2.COLOR_BGRA2GRAY)
 
-        findTemplate = cv2.matchTemplate(screen_gray, template, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(findTemplate)
+        findTemplate = cv2.matchTemplate(screenGray, template, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, _ = cv2.minMaxLoc(findTemplate)
+        if max_val > 0.5:
+            findFish = cv2.matchTemplate(screenGray, fish, cv2.TM_CCOEFF_NORMED)
+            _, maxFish, _, fishCoord = cv2.minMaxLoc(findFish)
 
+            findGreenZone = cv2.matchTemplate(screenGray, greenZone, cv2.TM_CCOEFF_NORMED)
+            _, maxGreen, _, greenCoord = cv2.minMaxLoc(findGreenZone)
+
+            greenY = greenCoord[1]
+            fishY = fishCoord[1]
+            print(maxGreen, greenY, maxFish, fishY)
+            if greenY - fishY > 0:
+                print("C")
+                useC(True)
+            else:
+                useC(False)
         
         cv2.imshow('Test', screen)
 
